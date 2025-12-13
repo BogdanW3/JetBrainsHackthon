d2f43db82299f6edb2ba9bd0007f85a681c26a52
Marko Jankovic <markojankovic2002@gmail.com>
2025-12-13 15:10:43 +0100

    indent groups

--- src/main/kotlin/rs/sljivicbusiness/jetbrainshackathon/regex/RegexExplainer.kt
+++ src/main/kotlin/rs/sljivicbusiness/jetbrainshackathon/regex/RegexExplainer.kt
@@ -2,31 +2,33 @@ package rs.sljivicbusiness.jetbrainshackathon.regex
 
 object RegexExplainer {
 
-    fun explain(tokens: List<RegexToken>): List<String> =
+    fun explain(tokens: List<RegexToken>, depth: Int = 0): List<String> =
         tokens.map { token ->
+            // html indent
+            val indent = "&nbsp;".repeat((depth + if (token is RegexToken.Quantifier) 1 else 0) * 4)
             when (token) {
-                is RegexToken.StartAnchor -> "^  Start of the string"
-                is RegexToken.EndAnchor -> "$  End of the string"
-                is RegexToken.DigitClass -> "\\d  A digit (0–9)"
-                is RegexToken.WordClass -> "\\w  A word character (a-z, A-Z, 0-9, _)"
-                is RegexToken.WhitespaceClass -> "\\s  A whitespace character (space, tab, newline)"
+                is RegexToken.StartAnchor -> "$indent^  Start of the string"
+                is RegexToken.EndAnchor -> "$indent$  End of the string"
+                is RegexToken.DigitClass -> "$indent\\d  A digit (0–9)"
+                is RegexToken.WordClass -> "$indent\\w  A word character (a-z, A-Z, 0-9, _)"
+                is RegexToken.WhitespaceClass -> "$indent\\s  A whitespace character (space, tab, newline)"
                 is RegexToken.CharClass -> when (token.value) {
-                    "." -> ".  Any character (wildcard)"
-                    else -> "${token.value}  Character class"
+                    "." -> "$indent.  Any character (wildcard)"
+                    else -> "$indent${token.value}  Character class"
                 }
                 is RegexToken.Quantifier -> when {
-                    token.min == 0 && token.max == null -> "*  Zero or more times"
-                    token.min == 1 && token.max == null -> "+  One or more times"
-                    token.min == 0 && token.max == 1 -> "?  Zero or one time (optional)"
-                    token.min == token.max -> "{${token.min}}  Exactly ${token.min} time(s)"
-                    token.max == null -> "{${token.min},}  At least ${token.min} time(s)"
-                    else -> "{${token.min},${token.max}}  Between ${token.min} and ${token.max} times"
+                    token.min == 0 && token.max == null -> "$indent*  Zero or more times"
+                    token.min == 1 && token.max == null -> "$indent+  One or more times"
+                    token.min == 0 && token.max == 1 -> "$indent?  Zero or one time (optional)"
+                    token.min == token.max -> "$indent{${token.min}}  Exactly ${token.min} time(s)"
+                    token.max == null -> "$indent{${token.min},}  At least ${token.min} time(s)"
+                    else -> "$indent{${token.min},${token.max}}  Between ${token.min} and ${token.max} times"
                 }
                 is RegexToken.Group -> {
-                    val innerExplanation = explain(token.tokens).joinToString("\n    ")
-                    "(\n    $innerExplanation\n)  Group of tokens"
+                    val innerExplanation = explain(token.tokens, depth + 1).joinToString("<br>")
+                    "$indent(<br>$innerExplanation<br>$indent)  Group of tokens"
                 }
-                is RegexToken.Literal -> "${token.value}  Literal character"
+                is RegexToken.Literal -> "$indent${token.value}  Literal character"
             }
         }
 }
