134f4e512ee022fe83cf5882a305ae057cb925cb
Bogdan Gligorijević <bogdang@nordeus.com>
2025-12-13 12:48:00 +0100

    Missing import and refactor action presentation

--- src/main/kotlin/rs/sljivicbusiness/jetbrainshackathon/action/ExplainRegexAction.kt
+++ src/main/kotlin/rs/sljivicbusiness/jetbrainshackathon/action/ExplainRegexAction.kt
@@ -2,10 +2,20 @@ package rs.sljivicbusiness.jetbrainshackathon.action
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import rs.sljivicbusiness.jetbrainshackathon.regex.*
+import rs.sljivicbusiness.jetbrainshackathon.ui.ExplanationPopup
 
-class ExplainRegexAction : AnAction("Explain Regex") {
+class ExplainRegexAction : AnAction() {
+
+    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
+
+    override fun update(e: AnActionEvent) {
+        val presentation = e.presentation
+        presentation.text = "Explain Regex"
+        presentation.isEnabledAndVisible = e.getData(CommonDataKeys.EDITOR) != null
+    }
 
     override fun actionPerformed(e: AnActionEvent) {
         val editor = e.getData(CommonDataKeys.EDITOR) ?: return
